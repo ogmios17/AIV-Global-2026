42ed3235ae1afe5ea1621f461b24633a56849a21
ogmios17 <131702262+ogmios17@users.noreply.github.com>
2026-02-01 04:23:44 +0100

    animations

--- AIV2026/Assets/Scripts/PlayerMoveInput.cs
+++ AIV2026/Assets/Scripts/PlayerMoveInput.cs
@@ -31,62 +31,100 @@ public class PlayerMoveInput : MonoBehaviour
 
     public void Attack(InputAction.CallbackContext ctx)
     {
-        if (moveState.P1Card == null)
-        {
-            binder.Jammer.FighterAnim.SetTrigger("Attack");
-            binder.Jammer.CardsAnim.SetTrigger("Attack");
-        }
         if (!ctx.performed) return;
         if (binder.Jammer.ChosenMove != null) return;
         if (binder.Jammer.PlayerType == PlayerType.Player1)
-            moveState.OnP1Received(attack);
-        else moveState.OnP2Received(attack);
-        
-        Debug.Log("Attack");
+        {
+            if (moveState.P1Card == null)
+            {
+                binder.Jammer.FighterAnim.SetTrigger("Attack");
+                binder.Jammer.CardsAnim.SetTrigger("Attack");
+            }
+            moveState.OnP1Received(shove);
+        }
+        else
+        {
+            if (moveState.P2Card == null)
+            {
+                binder.Jammer.FighterAnim.SetTrigger("Attack");
+                binder.Jammer.CardsAnim.SetTrigger("Attack");
+            }
+            moveState.OnP2Received(shove);
+            Debug.Log("Attack");
+        }
     }
 
     public void Block(InputAction.CallbackContext ctx)
     {
-        if (moveState.P1Card == null)
-        {
-            binder.Jammer.FighterAnim.SetTrigger("Block");
-            binder.Jammer.CardsAnim.SetTrigger("Block");
-        }
         if (!ctx.performed) return;
         if (binder.Jammer.ChosenMove != null) return;
         if (binder.Jammer.PlayerType == PlayerType.Player1)
-            moveState.OnP1Received(block);
-        else moveState.OnP2Received(block);
-        Debug.Log("Block");
+        {
+            if (moveState.P1Card == null)
+            {
+                binder.Jammer.FighterAnim.SetTrigger("Block");
+                binder.Jammer.CardsAnim.SetTrigger("Block");
+            }
+            moveState.OnP1Received(shove);
+        }
+        else
+        {
+            if (moveState.P2Card == null)
+            {
+                binder.Jammer.FighterAnim.SetTrigger("Block");
+                binder.Jammer.CardsAnim.SetTrigger("Block");
+            }
+            moveState.OnP2Received(shove);
+            Debug.Log("Block");
+        }
     }
 
     public void Grapple(InputAction.CallbackContext ctx)
     {
-        if (moveState.P1Card == null)
-        {
-            binder.Jammer.FighterAnim.SetTrigger("Grapple");
-            binder.Jammer.CardsAnim.SetTrigger("Grapple");
-        }
         if (!ctx.performed) return;
         if (binder.Jammer.ChosenMove != null) return;
         if (binder.Jammer.PlayerType == PlayerType.Player1)
-            moveState.OnP1Received(grapple);
-        else moveState.OnP2Received(grapple);
-        Debug.Log("Grapple");
+        {
+            if (moveState.P1Card == null)
+            {
+                binder.Jammer.FighterAnim.SetTrigger("Grapple");
+                binder.Jammer.CardsAnim.SetTrigger("Grapple");
+            }
+            moveState.OnP1Received(shove);
+        }
+        else
+        {
+            if (moveState.P2Card == null)
+            {
+                binder.Jammer.FighterAnim.SetTrigger("Grapple");
+                binder.Jammer.CardsAnim.SetTrigger("Grapple");
+            }
+            moveState.OnP2Received(shove);
+        }
     }
 
     public void Shove(InputAction.CallbackContext ctx)
     {
-        if (moveState.P1Card == null)
-        {
-            binder.Jammer.FighterAnim.SetTrigger("Shove");
-            binder.Jammer.CardsAnim.SetTrigger("Shove");
-        }
         if (!ctx.performed) return;
         if (binder.Jammer.ChosenMove != null) return;
         if (binder.Jammer.PlayerType == PlayerType.Player1)
+        {
+            if (moveState.P1Card == null)
+            {
+                binder.Jammer.FighterAnim.SetTrigger("Shove");
+                binder.Jammer.CardsAnim.SetTrigger("Shove");
+            }
             moveState.OnP1Received(shove);
-        else moveState.OnP2Received(shove);
+        }
+        else
+        {
+            if (moveState.P2Card == null)
+            {
+                binder.Jammer.FighterAnim.SetTrigger("Shove");
+                binder.Jammer.CardsAnim.SetTrigger("Shove");
+            }
+            moveState.OnP2Received(shove);
+        }
         Debug.Log("Shove");
     }
 
